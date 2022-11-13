e4e2448b984cc1dcd29b224e3ef50fcd93bbe11b
ACarmonaTS <ts.acarmona@outlook.com>
2022-11-13 01:30:53 -0600

    Menú de opciones funcional con Js

---
+++ javascript/index.js
@@ -0,0 +1,26 @@
+/*Si presiona el botón de "opciones", entonces se hace invisible, mientras que el 
+botón de "cancelar" se hace visible, así como el menú de opciones */
+function cancelar(){
+    document.getElementById('opc').style.display = "none";
+    document.getElementById('cancel').style.display = "block";
+    document.getElementById('listaHeader').style.visibility = "visible";
+}
+/*Si presiona el botón de "cancelar", entonces se hace invisible, mientras que el 
+botón de "opciones" se hace visible, el menú de opciones se vuelve invisible*/
+function opciones(){
+    document.getElementById('opc').style.display = "block";
+    document.getElementById('cancel').style.display = "none";
+    document.getElementById('listaHeader').style.visibility = "hidden";
+}
+/*Esta función detecta el cambio de tamaño de anchura del navegador, de esta forma establecemos 
+la visibilidad del menú de opciones, el botón de "cancelar" y el de "opciones"*/
+window.onresize = function() {
+    if(window.innerWidth>=1200){ 
+        document.getElementById('listaHeader').style.visibility = "visible";
+    }
+    else{ 
+        document.getElementById('listaHeader').style.visibility = "hidden";
+        document.getElementById('cancel').style.display = "none";
+        document.getElementById('opc').style.display = "block";
+    }
+};
