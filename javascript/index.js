df585e65cd191201a64725e2a2ab9eb1472d5342
ACarmonaTS <ts.acarmona@outlook.com>
2022-11-20 22:57:20 -0600

    Sección agregar sin local storage

--- javascript/index.js
+++ javascript/index.js
@@ -27,37 +27,50 @@ function seccionCrud( opcion ){
     document.getElementById('readV').style.display = "none";
     document.getElementById(opcion).style.display = "block";
     if(window.innerWidth<=1199){ opciones(); }
-    if( opcion == "readV"){ mostrar(); }
+    if( opcion == "readV"){ document.getElementById('tRead').innerHTML = mostrarLista(lista); }
 }
 /*=====================================================================*/
 /*=====================================================================*/
 /*                        FUNCIONES CRUD                               */
 /*=====================================================================*/
 /*=====================================================================*/
-/* Variables */
+/* Variables para la creación de la lista */
 let lista = [];
 let cont = 0;
+/* Variables auxiliares */
+let nombre = categoria = clave = id = desc = "";
+let stock = venta = compra = ganancia = 0;
+let newAdd = [ nombre, categoria, clave, id, stock, compra, venta, ganancia, desc ];
 
 /* Función para agregar */
 function agregar(){
-    const nombre = document.getElementById('nameAdd').value;
-    const categoria = document.getElementById('catAdd').value;
-    const clave = document.getElementById('claveProdAdd').value;
-    const stock = document.getElementById('stockAdd').value;
-    const compra = document.getElementById('compraAdd').value;
-    const venta = document.getElementById('ventaAdd').value;
-    const desc = document.getElementById('descAdd').value;
-    const id = nombre.charAt(0) + clave.charAt(0) + cont;
-    const ganancia = parseFloat(venta) - parseFloat(compra);
-    const newAdd = { nombre, categoria, clave, stock, compra, venta, desc, id, ganancia, };
-    lista.push(newAdd);
-    cont++;  
-    console.log(lista);
-    document.getElementById("identAdd").value = id;
-    document.getElementById("gananciaAdd").value = parseFloat(ganancia);
+    categoria = document.getElementById('catAdd').value;
+    stock = document.getElementById('stockAdd').value;
+    desc = document.getElementById('descAdd').value;
+    if( venta+compra+ganancia===0 || nombre+categoria+clave==="" || desc==="" ){ alert("Completa todos los campos!!!!"); }
+    else{
+        newAdd = [ nombre, categoria, clave, id, stock, compra, venta, ganancia, desc ];
+        lista.push(newAdd);
+        cont++;
+        limpiar();
+    }
     // document.getElementById("codigo").innerHTML = JSON.stringify(agenda, null, 3);
 }
-
+/*Esta función crea el Id y lo muestra*/
+function makeId(){
+    nombre = document.getElementById('nameAdd').value;
+    clave = document.getElementById('claveProdAdd').value;
+    id = nombre.charAt(0) + clave.charAt(0) + cont;
+    document.getElementById("identAdd").value = id;
+}
+/*Esta función calcula la ganancia y la muestra*/
+function makeGan(){
+    compra = document.getElementById('compraAdd').value;
+    venta = document.getElementById('ventaAdd').value;
+    ganancia = parseFloat(venta) - parseFloat(compra);
+    document.getElementById("gananciaAdd").value = parseFloat(ganancia);
+}
+/*Función para limpiar los campos del formulario*/
 function limpiar(){
     document.getElementById('nameAdd').value = "";
     document.getElementById('catAdd').value = "";
@@ -68,4 +81,39 @@ function limpiar(){
     document.getElementById('ventaAdd').value = 0;
     document.getElementById('gananciaAdd').value = 0;
     document.getElementById('descAdd').value = "";
+}
+/*Muestreo en tabla de datos principales*/
+function mostrarLista(list){
+    let compList = "<thead><tr><th>ID</th><th>Nombre</th><th>Stock</th><th>Precio<br>Venta</th><th>Precio<br>Compra</th><th>Ganancia</th></tr></thead><tbody>";
+    let fila = "";
+    for( let dato=0; dato<list.length; dato++ ){
+        fila += "<tr><td>";
+        fila += list[dato][0];
+        fila += "</td>";
+
+        fila += "<td>";
+        fila += list[dato][1];
+        fila += "</td>";
+
+        fila += "<td>";
+        fila += list[dato][4];
+        fila += "</td>";
+
+        fila += "<td>";
+        fila += list[dato][6];
+        fila += "</td>";
+
+        fila += "<td>";
+        fila += list[dato][5];
+        fila += "</td>";
+
+        fila += "<td>";
+        fila += list[dato][7];
+        fila += "</td>";
+
+        fila += "</tr>";
+    }
+    fila += "</tbody>";
+    compList += fila;
+    return compList;
 }
